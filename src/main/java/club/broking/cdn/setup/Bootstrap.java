ab9a819fdadc35c53c066b0ca889e88432c8a634
Marc Vornetran <marc.vornetran@gmail.com>
2016-04-02 18:03:47 +0200

    Cassandra connector

--- src/main/java/club/broking/cdn/setup/Bootstrap.java
+++ src/main/java/club/broking/cdn/setup/Bootstrap.java
@@ -3,8 +3,10 @@ package club.broking.cdn.setup;
 public class Bootstrap {
 
     public void setup() {
+        Cassandra cassandra = new Cassandra();
         Jetty jetty = new Jetty();
 
+        cassandra.setup();
         jetty.setup();
     }
 
