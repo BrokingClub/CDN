88584b8d72f343383fc8e613d44395fb08614b00
Marc Vornetran <marc.vornetran@gmail.com>
2016-04-02 17:34:33 +0200

    Refactored userService

--- src/main/resources/assets/js/services/userService.js
+++ src/main/resources/assets/js/services/userService.js
@@ -1,15 +1,52 @@
 class UserService {
 
     constructor() {
+        this.token = null;
+        this.name = null;
+        this.admin = false;
         this.loggedIn = false;
-        this.isAdmin = false;
-        this.user = null;
+
+        this.init();
+    }
+
+    init() {
+        const userItem = localStorage.getItem('user');
+
+        try {
+            const user = JSON.parse(userItem);
+
+            this.token = user.token;
+            this.name = user.name;
+            this.admin = user.admin;
+            this.loggedIn = true;
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    login(token, name, admin) {
+        this.token = token;
+        this.name = name;
+        this.admin = admin;
+        const user = this.user();
+
+        localStorage.setItem('user', JSON.stringify(user));
     }
 
     logout() {
-        this.loggedIn = false;
-        this.isAdmin = false;
-        this.user = null;
+        this.token = null;
+        this.name = null;
+        this.admin = false;
+
+        localStorage.removeItem('user');
+    }
+
+    user() {
+        return {
+            token: this.token,
+            name: this.name,
+            admin: this.admin
+        };
     }
 
 }
