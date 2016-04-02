31ea22d0fff7e1d5d00bc12902f638d74ef0883f
Marc Vornetran <marc.vornetran@gmail.com>
2016-04-02 23:45:00 +0200

    Implemented homepage product overview

---
+++ src/main/resources/assets/js/components/views/Shop.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card-columns">
+        <div class="card" v-for="product in products">
+            <img class="card-img-top" :src="product.image" style="width: 100%">
+            <div class="card-block">
+                <h4 class="card-title">{{ product.name }}</h4>
+                <p class="card-text">Preis: {{ product.price }} &euro;</p>
+                <button type="button" class="btn btn-primary-outline" @click="buy(product)">
+                    Kaufen
+                    <i class="fa fa-shopping-basket"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/babel">
+    import apiService from '../../services/apiService';
+    import userService from '../../services/userService';
+
+    export default {
+        data() {
+            return {
+                products: []
+            };
+        },
+        methods: {
+            buy(product) {
+                if(!userService.user) {
+                    this.$route.router.go('/anmelden');
+                    return;
+                }
+            }
+        },
+        route: {
+            activate() {
+                return apiService.products()
+                    .then(response => {
+                        this.products = response.data.products;
+                    });
+            }
+        },
+    };
+</script>
